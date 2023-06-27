dbc50ec78df811de63f5cec230389cdaa9cc3a94
Albina Anistratenko <albuska2013@gmail.com>
2023-06-27 13:02:52 +0300

    finish

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,14 +4,21 @@ const {
 const { ctrlWrapper } = require("../../helpers");
 
 const listContacts = async (req, res) => {
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, search, order, sort } = req.query;
   const skip = (page - 1) * limit;
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner, favorite: true }, "", {
+
+  const result = await Contact.find({ owner, favorite: search  || true }, "", {
     skip,
     limit,
+  }).sort(`${order === "DESC" ? "-" : ""}${sort || "name"}`);
+
+  const total = await Contact.count({ owner, favorite: search || true });
+  
+  res.status(200).json({
+    result,
+    total,
   });
-  res.status(200).json(result);
 };
 
 module.exports = {
